13dfa51bb58c8db524bff938bfb28a5e124eb9be
MikeFeng <maifengyx@gmail.com>
2025-09-08 14:08:54 +0800

    feat: Bro I just leaked my file directory

--- Build-lazy.bat
+++ Build-lazy.bat
@@ -1,6 +1,4 @@
 @echo off
 
-cd C:\Users\MikeFeng\OneDrive\MFeee-New
-
 lune run Build minify=true darklua-config-path="Build/DarkluaNormal.json" output="Tests/Script.luau"
 lune run Build minify=true darklua-config-path="Build/DarkluaMinify.json" output="Tests/Script-Minified.luau"
