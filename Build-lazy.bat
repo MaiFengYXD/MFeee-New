70b96318d733560b91b9c3f6d3cda162da74616e
MaiFengYXD <maifengyx@gmail.com>
2025-09-09 22:19:20 +0800

    build: Fix the stupid batch code mistake

--- Build-lazy.bat
+++ Build-lazy.bat
@@ -1,4 +1,5 @@
 @echo off
 
-lune run Build minify=true darklua-config-path="Build/DarkluaNormal.json" output="Tests/Script.luau"
-lune run Build minify=true darklua-config-path="Build/DarkluaMinify.json" output="Tests/Script-Minified.luau"
+lune run Build bundle output="Tests/Script.luau"
+lune run Build bundle minify=true darklua-config-path="Build/DarkluaNormal.json" output="Tests/Script-Formatted.luau"
+lune run Build bundle minify=true darklua-config-path="Build/DarkluaMinify.json" output="Tests/Script-Minified.luau"
